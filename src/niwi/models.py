0105981cc90503f8894847b8a6fa5a9981e24beb
Andrei Antoukh <niwi@niwi.be>
2011-08-21 19:34:28 +0200

    Clear models.

--- src/niwi/models.py
+++ src/niwi/models.py
@@ -10,19 +10,33 @@ from niwi.paste.forms import LEXER_CHOICES
 from niwi.contrib.db.fields import UUIDField, CreationDateTimeField, \
     ModificationDateTimeField, AutoSlugField
 
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
+
 import datetime, uuid
 
 STATUS_CHOICES = (
     ('public', u'Public'),
     ('private', u'Private'),
     ('draft', u'Draft'),
-    ('deleted', u'Deleted'),
-    ('versioned', u'Versioned'),
 )
 
-class Document(models.Model):
-    uuid = UUIDField(unique=True, db_index=True, editable=True)
-    slug = AutoSlugField(populate_from='title', unique=True, db_index=True, editable=True)
+def slugify_uniquely(value, model, slugfield="slug"):
+    suffix = 0
+    potential = base = slugify(value)
+    if len(potential) == 0:
+        potential = 'null'
+    while True:
+        if suffix:
+            potential = "-".join([base, str(suffix)])
+        if not model.objects.filter(**{slugfield: potential}).count():
+            return potential
+        suffix += 1
+
+
+class Page(models.model):
+    slug  = models.SlugField(unique=True, db_index=True, editable=True)
+    #slug = AutoSlugField(populate_from='title', unique=True, db_index=True, editable=True)
     title = models.CharField(max_length=500, db_index=True)
     content = models.TextField()
     markup = models.BooleanField(default=False)
@@ -35,69 +49,47 @@ class Document(models.Model):
     current_version = models.IntegerField(default=1)
     parent_version = models.IntegerField(null=True, default=None, blank=True)
     content_type = models.ForeignKey(ContentType, null=True, default=None)
-
-
     class Meta:
-        db_table = 'document'
-
-    def save(self, *args, **kwargs):
-        if not self.content_type:
-            self.content_type = ContentType.objects.get_for_model(self.__class__)
-
-        super(Document, self).save(*args, **kwargs)
-
-    def as_leaf_class(self):
-        model = self.content_type.model_class()
-
-        if model == self.__class__:
-            return self
-        else:
-            return model.objects.get(pk=self.id)
-
-    def __unicode__(self):
-        return self.title
-
-
-class WebFile(models.Model):
-    uuid = UUIDField(unique=True, db_index=True, editable=True)
-    document = models.ForeignKey('Document', related_name='files')
-    file = models.FileField(upload_to='wf/%Y/%m/%d')
-
-    def __unicode__(self):
-        return self.uuid
-
-    class Meta:
-        db_table = 'webfile'
-
-
-class Page(Document):
-    class Meta:
-        db_table = 'document_page'
+        db_table = 'pages'
 
     @models.permalink
     def get_absolute_url(self):
         return ('web:show-page', (), {'slug': self.slug})
     
     def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_uniquely(self.title, self.__class__)
+
         super(Page, self).save(*args, **kwargs)
 
 
-class Post(Document):
+class Post(models.Model):
+    slug  = models.SlugField(unique=True, db_index=True, editable=True)
+    #slug = AutoSlugField(populate_from='title', unique=True, db_index=True, editable=True)
+    title = models.CharField(max_length=500, db_index=True)
+    content = models.TextField()
+    markup = models.BooleanField(default=False)
+    status = models.CharField(max_length=40, choices=STATUS_CHOICES, db_index=True, default='draft')
+
+    created_date = CreationDateTimeField(editable=True)
+    modified_date = ModificationDateTimeField(editable=True)
+
+    current_version = models.IntegerField(default=1)
+    parent_version = models.IntegerField(null=True, default=None, blank=True)
+    content_type = models.ForeignKey(ContentType, null=True, default=None)
+
     class Meta:
-        db_table = 'document_post'
+        db_table = 'posts'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_uniquely(self.title, self.__class__)
+
+        super(Post, self).save(*args, **kwargs)
 
     @models.permalink
     def get_absolute_url(self):
         return ('web:show-post', (), {'slug': self.slug})
-
-
-class Project(Document):
-    @models.permalink
-    def get_absolute_url(self):
-        return ('web:show-project', (), {'slug': self.slug})
-
-    class Meta:
-        db_table = 'project'
 
 
 class Link(models.Model):
@@ -111,7 +103,7 @@ class Link(models.Model):
     public = models.BooleanField(default=True)
 
     class Meta:
-        db_table = 'link'
+        db_table = 'links'
 
     def save(self, *args, **kwargs):
         if not self.title:
@@ -125,18 +117,6 @@ class Link(models.Model):
     @models.permalink
     def get_absolute_url(self):
         return ('web:show-link', (), {'slug': self.slug})
-
-
-class LinkCache(models.Model):
-    link = models.ForeignKey('Link', related_name='cache')
-    date = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
-
-    class Meta:
-        db_table = 'link_cache'
-
-    def __unicode__(self):
-        return u"%s/%s" % (self.link.slug, unicode(self.date))
 
 
 class Config(models.Model):
