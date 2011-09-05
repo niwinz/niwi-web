988dc3749186919d7e6866b6b17f29d09b0a61d5
Andrei Antoukh <niwi@niwi.be>
2011-09-06 00:14:34 +0200

    Add some new settings.

--- src/niwi/context.py
+++ src/niwi/context.py
@@ -10,6 +10,10 @@ def main(request):
         fcbk_admin = settings.FCBK_ADMIN,
         fcbk_app_id = settings.FCBK_APP_ID,
         host = settings.HOST,
+        page_default_logo_url = settings.PAGE_DEFAULT_LOGO_URL,
+        page_default_description = settings.PAGE_DEFAULT_DESCRIPTION,
+        page_default_keyworkds = settings.PAGE_DEFAULT_KEYWORKDS,
+        page_default_title = settings.PAGE_DEFAULT_TITLE
     )
     context_extra['full_current_url'] = context_extra['current_url']
     if request.META.get("QUERY_STRING"):
