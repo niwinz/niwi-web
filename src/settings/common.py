914c4652c33f3d81ce6777e3d6f9165dde3b1f46
Andrei Antoukh <niwi@niwi.be>
2011-09-06 00:44:17 +0200

    Add fotmats and improved bookmark page with formats.

--- src/settings/common.py
+++ src/settings/common.py
@@ -184,3 +184,8 @@ LOGGING = {
         }
     }
 }
+
+FORMAT_MODULE_PATH = 'niwi.formats'
+DATE_INPUT_FORMATS = ('%Y-%m-%d', '%m/%d/%Y', '%d/%m/%Y', '%b %d %Y',
+'%b %d, %Y', '%d %b %Y', '%d %b, %Y', '%B %d %Y',
+'%B %d, %Y', '%d %B %Y', '%d %B, %Y')
