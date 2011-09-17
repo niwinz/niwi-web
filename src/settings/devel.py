fc2a5754794ac20e16ac4145a052f3aa07b52d27
Andrei Antoukh <niwi@niwi.be>
2011-09-17 16:26:03 +0200

    Remove memcached cache default settings and add requirements.txt

--- src/settings/devel.py
+++ src/settings/devel.py
@@ -5,24 +5,15 @@ from .common import *
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
-ADMINS = (
-    ('Andrei Antoukh', 'niwi@niwi.be'),
-)
-
-MANAGERS = ADMINS
 USE_ETAGS=False
-SESSION_ENGINE='django.contrib.sessions.backends.cache'
-SESSION_SAVE_EVERY_REQUEST = False
-SESSION_COOKIE_AGE = 1209600 # (2 weeks)
-
 
 TEMPLATE_CONTEXT_PROCESSORS += [
     "django.core.context_processors.debug",
 ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
+#CACHES = {
+#    'default': {
+#        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#        'LOCATION': '127.0.0.1:11211',
+#    }
+#}
