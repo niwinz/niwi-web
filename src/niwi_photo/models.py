e3649b8542a669898335d2cc8b4dbec59a60a23c
Andrei Antoukh <niwi@niwi.be>
2011-09-21 00:06:38 +0200

    Move resize images process to sigdispatch module.

--- src/niwi_photo/models.py
+++ src/niwi_photo/models.py
@@ -11,10 +11,7 @@ from niwi.contrib.db.fields import ModificationDateTimeField
 from niwi.contrib.db.fields import DictField
 
 from niwi.models import slugify_uniquely
-from niwi_photo.image import ImageAdapter
-
 import datetime, uuid
-import tempfile
 
 class Album(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -44,19 +41,4 @@ class Photo(models.Model):
         if not self.slug:
             self.slug = slugify_uniquely(self.small_description, self.__class__)
 
-        super(Photo, self).save(*args, **kwargs)
-        f1, f2, f3 = None, None, None
-
-        if not self.large:
-            f1 = tempfile.NamedTemporaryFile(suffix=".jpg", delete=True)
-            self.large = ImageAdapter.resize(self.original.path, f1, 1200)
-
-        if not self.medium:
-            f2 = tempfile.NamedTemporaryFile(suffix=".jpg", delete=True)
-            self.medium = ImageAdapter.resize(self.original.path, f2, 900)
-
-        if not self.square:
-            f3 = tempfile.NamedTemporaryFile(suffix=".jpg", delete=True) 
-            self.square = ImageAdapter.square(self.original.path, f3)
-        
         super(Photo, self).save(*args, **kwargs)
---
+++ src/niwi_photo/sigdispatch.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from django.db.models.signals import pre_save, post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+from niwi_photo.models import Photo
+from niwi_photo.image import ImageAdapter
+
+import tempfile
+
+@receiver(post_save, sender=Photo)
+def photo_post_save(sender, instance, created, **kwargs):
+    """ Create resized images for photo instance. """
+
+    if not instance.large:
+        f1 = tempfile.NamedTemporaryFile(suffix=".jpg", delete=True)
+        instance.large = ImageAdapter.resize(instance.original.path, f1, 1200)
+
+    if not instance.medium:
+        f2 = tempfile.NamedTemporaryFile(suffix=".jpg", delete=True)
+        instance.medium = ImageAdapter.resize(instance.original.path, f2, 900)
+
+    if not instance.square:
+        f3 = tempfile.NamedTemporaryFile(suffix=".jpg", delete=True) 
+        instance.square = ImageAdapter.square(instance.original.path, f3)
+
+    instance.save()
