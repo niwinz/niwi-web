850f93495e8e64d26926e5419f1ce7ed677b3a84
Andrei Antoukh <niwi@niwi.be>
2011-09-21 11:46:39 +0200

    Add colorbox and improved niwi_photo index page.

--- src/niwi/static/js/base.js
+++ src/niwi/static/js/base.js
@@ -1,4 +1,9 @@
+var niwiPhoto_bindings = function() {
+    $('article.photo a[rel="photo"]').colorbox({ width:"75%", height: "95%"});
+};
+
 $(document).ready( function() {
+    niwiPhoto_bindings();
     $('.langcode').click( function(){
         var selected_lang = $(this).attr('rel');
 
