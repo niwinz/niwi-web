f6ee7ec0251bf7331abb77015c937ecee7f695f1
Andrei Antoukh <niwi@niwi.be>
2011-09-27 18:06:30 +0200

    Add some files and delete other.

---
+++ src/niwi_apps/filepaste/urls.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
+from django.views.generic import TemplateView, RedirectView
+
+from niwi_apps.filepaste.views import *
+
+urlpatterns = patterns('',
+)
---
+++ src/niwi_apps/filepaste/views.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.views.decorators.cache import cache_page
+
+from niwi.views.generic import GenericView
