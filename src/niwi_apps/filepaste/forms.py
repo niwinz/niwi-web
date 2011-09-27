cbb4ee3fc512fab637c698c5b9b3d941c2fac4ac
Andrei Antoukh <niwi@niwi.be>
2011-09-27 20:18:43 +0200

    Implemented password file download.

--- src/niwi_apps/filepaste/forms.py
+++ src/niwi_apps/filepaste/forms.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from django.utils.translation import ugettext_lazy as _
+
 from django import forms
 from django.forms import ModelForm
 from niwi_apps.filepaste.models import WebFile
@@ -10,3 +12,19 @@ class UploadForm(ModelForm):
         model = WebFile
         fields = ('description', 'attached_file', \
                     'password', 'hidden')
+
+class DownloadForm(forms.Form):
+    password = forms.CharField(max_length=200, widget=forms.PasswordInput)
+
+    def __init__(self, *args, **kwargs):
+        self.wfile = kwargs.pop('wfile')
+        super(DownloadForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        if "password" in cleaned_data:
+            if not self.wfile.check_password(cleaned_data['password']):
+                self._errors['password'] = self.error_class([_(u'Incorrect password')])
+                del cleaned_data['password']
+            
--- src/niwi_apps/filepaste/views.py
+++ src/niwi_apps/filepaste/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.utils.translation import ugettext_lazy as _
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.cache import cache_page
 from django.core.urlresolvers import reverse
@@ -7,7 +8,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 
 from niwi.views.generic import GenericView
 from niwi_apps.filepaste.models import WebFile
-from niwi_apps.filepaste.forms import UploadForm
+from niwi_apps.filepaste.forms import UploadForm, DownloadForm
 import os.path
 
 class WebFileDownload(GenericView):
@@ -20,10 +21,23 @@ class WebFileDownload(GenericView):
             response['Content-Disposition'] = 'attachment; filename=%s' % \
                 os.path.basename(wfile.attached_file.name)
             return response
+            
+        form = DownloadForm(wfile=wfile)
+        return self.render_to_response(self.template_name,
+            {'form':form, 'file':wfile})
 
-        else:
-            return self.render_to_response(self.template_name)
-
+    def post(self, request, slug):
+        wfile = get_object_or_404(WebFile, slug=slug)
+        form = DownloadForm(request.POST, wfile=wfile)
+        if form.is_valid():
+            response = HttpResponse(mimetype="application/octet-stream")
+            response['X-Accel-Redirect'] = wfile.attached_file.url
+            response['Content-Disposition'] = 'attachment; filename=%s' % \
+                os.path.basename(wfile.attached_file.name)
+            return response
+        
+        return self.render_to_response(self.template_name,
+            {'form': form, 'file':wfile})
 
 class WebFileUpload(GenericView):
     template_name = "filepaste_upload.html"
@@ -35,7 +49,11 @@ class WebFileUpload(GenericView):
     def post(self, request):
         form = UploadForm(request.POST, request.FILES)
         if form.is_valid():
-            wfile = form.save()
+            wfile = form.save(commit=False)
+            if form.cleaned_data['password']:
+                wfile.set_password(form.cleaned_data['password'])
+
+            wfile.save()
             return HttpResponseRedirect(reverse("web:show-home"))
 
         return self.render_to_response(self.template_name, {'form':form})
