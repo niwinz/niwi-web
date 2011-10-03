1be7798008d6ad988e465709e2bf1dbc742f9ed1
Andrei Antoukh <niwi@niwi.be>
2011-10-03 21:40:31 +0200

    Implemented default full _cursor method.

--- src/niwi_apps/db/postgresql_psycopg2/base.py
+++ src/niwi_apps/db/postgresql_psycopg2/base.py
@@ -25,25 +25,60 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         self.server_side_cursors = False
         self.server_side_cursor_itersize = None
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
-    
+
     def _cursor(self):
         """
         Returns a unique server side cursor if they are enabled, 
         otherwise falls through to the default client side cursors.
         """
-        if self.server_side_cursors:
-            # intialise the connection if we haven't already
-            # this will waste a client side cursor, but only on the first call
-            if self.connection is None:
-                super(DatabaseWrapper, self)._cursor()
+        new_connection = False
+        set_tz = False
+        settings_dict = self.settings_dict
+
+        if self.connection is None:
+            new_connection = True
+            set_tz = settings_dict.get('TIME_ZONE')
+
+            if settings_dict['NAME'] == '':
+                from django.core.exceptions import ImproperlyConfigured
+                raise ImproperlyConfigured("You need to specify NAME in your"
+                                           " Django settings file.")
             
+            conn_params = {
+                'database': settings_dict['NAME'],
+            }
+            conn_params.update(settings_dict['OPTIONS'])
+            if 'autocommit' in conn_params:
+                del conn_params['autocommit']
+
+            if settings_dict['USER']:
+                conn_params['user'] = settings_dict['USER']
+            if settings_dict['PASSWORD']:
+                conn_params['password'] = settings_dict['PASSWORD']
+            if settings_dict['HOST']:
+                conn_params['host'] = settings_dict['HOST']
+            if settings_dict['PORT']:
+                conn_params['port'] = settings_dict['PORT']
+            self.connection = Database.connect(**conn_params)
+            self.connection.set_client_encoding('UTF8')
+            self.connection.set_isolation_level(self.isolation_level)
+            connection_created.send(sender=self.__class__, connection=self)
+
+        cursor = None
+        if self.server_side_cursors:
             # give the cursor a unique name which will invoke server side cursors
             cursor = self.connection.cursor(name='cur%s' % str(uuid.uuid4()).replace('-', ''))
             cursor.tzinfo_factory = None
             
             if self.server_side_cursor_itersize is not None:
                 cursor.itersize = self.server_side_cursor_itersize
+        else:
+            cursor = self.connection.cursor()
+            cursor.tzinfo_factory = None
             
-            return CursorWrapper(cursor)
-        
-        return super(DatabaseWrapper, self)._cursor()
+        if new_connection:
+            if set_tz:
+                cursor.execute("SET TIME ZONE %s", [settings_dict['TIME_ZONE']])
+            self._get_pg_version()
+
+        return CursorWrapper(cursor)
